58eb625be7f731536562a3efdabe61fc5c7f8783
Robin Allen <114621789+robinallenaz@users.noreply.github.com>
2024-10-10 08:39:19 -0700

    Worked on building adventurer object with nested objects and roll function

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("Hello World");
+//Part 1: Humble Beginnings
+
+const adventurer = {
+  name: "Robin",
+  health: 10,
+  inventory: ["sword", "potion", "artifact"],
+  //part 1A
+  companion: {
+    name: "Leo",
+    type: "Cat",
+    companion: {
+        name: "Frank",
+        type: "Flea",
+        inventory: ["small hat", "sunglasses"]
+    }
+  }
+};
+
+roll (mod = 0) {
+    const result = Math.floor(Math.random() * 20) + 1 + mod;
+    console.log(`${this.name} rolled ${result}`);
+}
+for (let i=0; i<adventurer.inventory.length; i++) {
+    console.log(adventurer.inventory[i]);
+
+}
+console.log(adventurer.companion.companion.inventory[1]);
+
+adventurer.roll()
