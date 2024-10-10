d599e2817f3e8b90e5f7b82e94b378bb3cdb3780
Robin Allen <114621789+robinallenaz@users.noreply.github.com>
2024-10-10 08:46:51 -0700

    Created Character class and set properties for new character

--- script.js
+++ script.js
@@ -26,4 +26,28 @@ for (let i=0; i<adventurer.inventory.length; i++) {
 }
 console.log(adventurer.companion.companion.inventory[1]);
 
-adventurer.roll()
+adventurer.roll();
+
+//Part 2
+
+class Character {
+    constructor(name) {
+        this.name = name;
+        this.health = 100;
+        this.inventory = [];
+
+    }
+}
+roll (mod = 0) {
+    const result = Math.floor(Math.random() * 20) + 1 + mod;
+    console.log(`${this.name} rolled ${result}`);
+}
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
+
+console.log(robin);
